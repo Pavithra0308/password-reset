253ad56fef3395cf273fee78421cf8863bfc9c0c
Pavithra0308 <puppypavi.03081998@gmail.com>
2025-07-12 20:54:58 +0530

    commit

--- Server/index.js
+++ Server/index.js
@@ -22,19 +22,10 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Origin",
-      req.headers.origin || process.env.CLIENT_URL
-    );
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS"
-    );
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    return res.status(200).end();
-  }
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; img-src 'self' data:; style-src 'self'; font-src 'self';"
+  );
   next();
 });
 
