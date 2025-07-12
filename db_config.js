809e72d3bcec8fc2b7ef709dfdbcecdc6e9a9cad
Pavithra0308 <puppypavi.03081998@gmail.com>
2025-07-12 19:49:59 +0530

    first commit

--- db_config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => {
-    console.error("Connection error", err);
-    process.exit(1);
-  });
